e4ca2d3ed33de7ff0654b10511df70ce6330e0f7
26sharanya <128603893+26sharanya@users.noreply.github.com>
2024-05-22 18:24:24 +0530

    Add files via upload

---
+++ Vehicle_oops/car.java
@@ -0,0 +1,19 @@
+package Vehicle_oops;
+
+public class car extends Vehicle{
+	
+	private String type;
+	
+	public car(int a, int b, String c, String t, String f) {
+		super(a, b, c);
+		this.type=t;
+	}
+
+	
+	void CarOutput() {
+		super.VehicleOutput();
+		System.out.println("Fuel type = "+this.type);
+	}
+	
+
+}
